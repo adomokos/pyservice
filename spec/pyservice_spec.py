984bb6b7e4478c4c5e79331e31ca72732cca1880
Attila Domokos <adomokos@gmail.com>
2020-09-22 14:34:55 -0500

    Switch to using mamba

---
+++ spec/pyservice_spec.py
@@ -0,0 +1,7 @@
+from pyservice import __version__
+from mamba import description, it
+
+
+with description('Version') as self:
+    with it('verifies the package version'):
+        assert __version__ == '0.1.0'
