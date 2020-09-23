efe6b21a83bec3ade0fbebd42f66041c32502a1b
Attila Domokos <adomokos@gmail.com>
2020-09-22 22:15:55 -0500

    Use a metaclass to protect instance variables from failed state

--- pyservice/action.py
+++ pyservice/action.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from functools import wraps
 from pyservice.context import Context
 from typing import Callable
@@ -14,3 +15,34 @@ def action():
 
         return decorated
     return action_wrapper
+
+
+def verify_context(func):
+    '''
+        Guards the exeuction of the action, if the provided context
+        is in a failure state execution is stopped
+    '''
+    def wrapper(*args, **kwargs):
+        (cls, ctx) = args
+
+        if (ctx.is_success()):
+            func(*args, **kwargs)
+
+    return wrapper
+
+
+class ActionMeta(type):
+
+    def __init__(cls, name, bases, attrs, **kwargs):
+        super().__init__(name, bases, attrs)
+        cls.execute = verify_context(cls.execute)
+
+
+class Action(metaclass=ActionMeta):
+
+    @abstractmethod
+    def execute(self, ctx: Context) -> Context:
+        pass
+
+    def rollback(self, ctx: Context) -> Context:
+        pass
